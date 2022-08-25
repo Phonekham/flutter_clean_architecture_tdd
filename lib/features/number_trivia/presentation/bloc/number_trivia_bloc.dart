e76847f224aae4385e240d62a7aef9693a096db3
Phonekham <phone.madridista@gmail.com>
2022-08-26 00:30:39 +0700

    Bloc Scaffolding & Input Conversion

---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -0,0 +1,15 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_clean_architecture_tdd/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:meta/meta.dart';
+
+part 'number_trivia_event.dart';
+part 'number_trivia_state.dart';
+
+class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  NumberTriviaBloc() : super(NumberTriviaInitial()) {
+    on<NumberTriviaEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
@@ -0,0 +1,42 @@
+part of 'number_trivia_bloc.dart';
+
+@immutable
+abstract class NumberTriviaState extends Equatable {
+  const NumberTriviaState([List props = const <dynamic>[]]) : super();
+}
+
+class Empty extends NumberTriviaState {
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
+
+class NumberTriviaInitial extends NumberTriviaState {
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
+
+class Loading extends NumberTriviaState {
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
+
+class Loaded extends NumberTriviaState {
+  final NumberTrivia trivia;
+  Loaded({required this.trivia}) : super([trivia]);
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
+
+class Error extends NumberTriviaState {
+  final String message;
+  Error({required this.message}) : super([message]);
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
